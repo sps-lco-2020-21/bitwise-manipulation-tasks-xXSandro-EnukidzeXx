a6bf877376b0640c22f2bc2e1ff046a31da2db83
xXSandro-EnukidzeXx <72739547+xXSandro-EnukidzeXx@users.noreply.github.com>
2020-11-03 09:43:07 +0000

    Bitwise operators

---
+++ bitwise.App/bitwise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace bitwise
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Get the rightmost bit
+            /*int x = Convert.ToInt32(Console.ReadLine(), 2);
+            Console.WriteLine(Convert.ToString(x & 1),2);*/
+
+            //Get the rightmost 3 bits
+            int y = Convert.ToInt32(Console.ReadLine(), 2);
+            Console.WriteLine(Convert.ToString(y & 111), 2);
+        }
+    }
+}
